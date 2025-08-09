66f3c87e95b0727fd7a61b0302ce523fc97071db
Junghoon Ban <junghoon.ban@gmail.com>
2025-08-09 23:35:54 +0900

    chore: Add architecture-specific URLs and checksums for Jindo formula

--- Formula/jindo.rb
+++ Formula/jindo.rb
@@ -1,8 +1,15 @@
 class Jindo < Formula
   desc "Bringing git-hooks into JVM world 🐕"
   homepage "https://github.com/jindo-io"
-  url "https://github.com/jindo-io/jindo/releases/download/v0.1.0-SNAPSHOT/jindo-aarch64-apple-darwin.zip"
-  sha256 "93e1f1ab7e6d63d95bc6a9c6a7b3aa282c08251d6e18ee9b40f71a83568e8aad"
+  on_arm do
+    url "https://github.com/jindo-io/jindo/releases/download/v0.1.0-SNAPSHOT/jindo-aarch64-apple-darwin.zip"
+    sha256 "390cb445de28d0a6d811021cbc7e35c6c03c6bba82b8b3135e75dff9bc6ede8f"
+  end
+
+  on_intel do
+    url "https://github.com/jindo-io/jindo/releases/download/v0.1.0-SNAPSHOT/jindo-x86_64-apple-darwin.zip"
+    sha256 "bb230f04f14bd0d595a452dc3161aa4e059f09eacced69c82195f0a0c1ed92c7"
+  end
   version "0.1.0-SNAPSHOT"
 
   def install
